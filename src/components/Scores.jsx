af0ce4604c511ab2571dee58f3b323baf597c547
Gonzalez32 <3232rose@gmail.com>
2021-01-05 14:10:42 -0600

    render data

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,18 +1,3 @@
-// import React from "react";
-
-// function Scores(props) {
-//   return (
-//     <div>
-//       {props.scores.map((s, idx) => (
-//         <p key={idx}>
-//           Date:{s.date} <br/> Score:{s.score}
-//     ))}
-//     </div>
-//   );
-// }
-
-// export default Scores;
-
 import React from "react";
 
 const Scores = (props) => {
--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 import Scores from "../components/Scores";
 
-// function Students(props) {
-//   return (
-//     <React.Fragment>
-//       {props.students.name}
-//       {props.students.bio}
-//       <Scores scores={props.studnets.scores} />
-//     </React.Fragment>
-//   );
-// }
-
 const Students = (props) => {
   return (
     <div>
